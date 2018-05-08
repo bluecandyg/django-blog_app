a660890cb59a4ed0a4693d091b7f98696a0ad6c9
bluecandyg <bluecandyg@naver.com>
2018-05-08 17:24:48 +0900

    update project

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),      # 메인 화면
+    # localhost:8000/post/1
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),     # 상세 페이지
 ]
