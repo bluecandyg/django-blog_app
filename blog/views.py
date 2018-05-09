1867d3a135f73239d36f10c34af241783b11fb6c
bluecandyg <bluecandyg@naver.com>
2018-05-09 11:11:29 +0900

    새글 등록 모듈 추가 Postgres 연동 모듈 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from blog.modelforms import PostModelForm
 from .models import Post
 
 # Create your views here.
@@ -21,3 +23,19 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+# 새글 등록
+def post_new(request):
+    if request.method == 'POST':
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
